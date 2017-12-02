c5b2de7d0c631fafc4cf68c4045272b63e868da2
Markus Reiter <me@reitermark.us>
2017-12-02 05:35:18 +0100

    Fix missing quotes.

--- functions/ruby_version.fish
+++ functions/ruby_version.fish
@@ -1,6 +1,6 @@
 function ruby_version
   if set -q RUBY_VERSION
-    printf $RUBY_VERSION
+    printf "$RUBY_VERSION"
     return 0
   else if command -s rbenv > /dev/null
     set -l rbenv_root (rbenv root)

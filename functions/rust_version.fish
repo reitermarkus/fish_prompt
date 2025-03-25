d2b7f4ca45c360ce8edb599d99183e6b1d16f1d3
Markus Reiter <me@reitermark.us>
2025-03-25 14:10:46 +0100

    Install Rust toolchain on demand.

--- functions/rust_version.fish
+++ functions/rust_version.fish
@@ -1,9 +1,24 @@
+function __rustc_version
+  if set -l rustc (command rustup which rustc 2>/dev/null)
+    printf (string split ' ' ($rustc --version))[2]
+    return 0
+  else
+    return 1
+  end
+end
+
 function __rust_version_rec
     set -l current_dir $argv[1]
 
     if test -f "$current_dir/Cargo.toml"
-        printf (string split ' ' (eval (rustup which rustc) --version))[2]
-        return 0
+        if set -l rustc_version (__rustc_version)
+          printf "$rustc_version"
+          return $status
+        else
+          command rustup install
+          __rustc_version
+          return $status
+        end
     end
 
     if test "$current_dir" = /
